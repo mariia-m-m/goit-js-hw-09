9ec5a79aab16d86e9feda1b17acb7c84a9aed198
Mariia Makarkina <mariiamak25@gmail.com>
2022-12-05 01:51:34 +0200

    Виконує першу задачку

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+const body = document.querySelector('body')
 
+btnStart.addEventListener("click", onBtnStart)
+btnStop.addEventListener('click', onBtnStop)
+let Interval = null;
+
+
+function onBtnStart() {
+    Interval =
+            setInterval(() =>
+                body.style.backgroundColor = getRandomHexColor(), 1000);
+        
+        function getRandomHexColor() {
+            return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+        }
+    btnStart.setAttribute('disabled', true);
+btnStop.removeAttribute('disabled')
+}
+
+
+function onBtnStop() {
+    clearInterval(Interval);
+btnStart.removeAttribute('disabled');
+btnStop.setAttribute('disabled',true)
+}
+ 
